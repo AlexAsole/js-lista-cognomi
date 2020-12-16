b820e75b00bd8bd931e5f5ee4f1dda45d13734b2
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2020-12-16 15:49:04 +0100

    inserimento cognome utente e conteggio posizione (il console)

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
+var userCognome = prompt('Salve, scriva il suo cognome(prima lettera maiuscola)');
 var lista = document.getElementById('lista');
 var listaCognomi = ['Rossi', 'Bianchi', 'Vieri', 'Pazzi', 'Zona', 'Innocenti', 'Flesso', 'Capatonda']
-
+listaCognomi.push(userCognome); //inserimento cognome utente tramite prompt
+// var posizione = listaCognomi.indexOf(userCognome) + 1; //posizione in lista non sortata
 listaCognomi.sort(); //Sorta in ordine alfabetico
+var posizione = listaCognomi.indexOf(userCognome) + 1; //posizione in lista sortata
 
 //Ciclo con for
 for (var i = 0; i < listaCognomi.length; i++) {
   lista.innerHTML += '<li>' + listaCognomi[i] + '</li>';
 }
-
+console.log(posizione);
 //Ciclo con while
 // var i = 0;
 // while (i < listaCognomi.length) {
