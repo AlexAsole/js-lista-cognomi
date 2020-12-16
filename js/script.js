5478aa0025e91d610920e83726e9cead6b2cdf80
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2020-12-16 15:59:07 +0100

    Conteggio posizione in pagina (reale in console)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 var userCognome = prompt('Salve, scriva il suo cognome(prima lettera maiuscola)');
 var lista = document.getElementById('lista');
+var posizione = document.getElementById('posizione');
 var listaCognomi = ['Rossi', 'Bianchi', 'Vieri', 'Pazzi', 'Zona', 'Innocenti', 'Flesso', 'Capatonda']
 listaCognomi.push(userCognome); //inserimento cognome utente tramite prompt
-// var posizione = listaCognomi.indexOf(userCognome) + 1; //posizione in lista non sortata
+var realPosition = listaCognomi.indexOf(userCognome) + 1; //posizione in lista non sortata
 listaCognomi.sort(); //Sorta in ordine alfabetico
-var posizione = listaCognomi.indexOf(userCognome) + 1; //posizione in lista sortata
+var position = listaCognomi.indexOf(userCognome) + 1; //posizione in lista sortata
 
 //Ciclo con for
 for (var i = 0; i < listaCognomi.length; i++) {
   lista.innerHTML += '<li>' + listaCognomi[i] + '</li>';
 }
-console.log(posizione);
+
 //Ciclo con while
-// var i = 0;
+var i = 0;
 // while (i < listaCognomi.length) {
 //   lista.innerHTML += '<li>' + listaCognomi[i] + '</li>';
 //   i++;
@@ -23,3 +24,7 @@ console.log(posizione);
 //   lista.innerHTML += '<li>' + listaCognomi[i] + '</li>';
 //    i++;
 // } while (i < listaCognomi.length);
+
+posizione.innerHTML = 'La tua posizione nella lista è: ' + position + '!'; //conteggio in pagina, ordine alfabetico
+console.log('La tua vera posizione, senza sort per ordine alfabetico è: ' + realPosition + '!' );
+//conteggio in console, ordine di inserimento
